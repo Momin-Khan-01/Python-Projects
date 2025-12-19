adccae603d321a865bba077982ed2846a2078465
Momin Khan <hmd84830@gmail.com>
2025-12-19 22:17:35 +0530

    Add Project

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,10 @@
+rent = int(input("enter your rent = "))
+food = int(input("enter your food order = "))
+electricity_bill = int(input("enter your bill = "))
+charge_per_unit = int(input("enter your charge_per_unit = "))
+persons = int(input("enter persons in living in room = "))
+
+total_bill = electricity_bill * charge_per_unit
+output = (food + rent + total_bill) // persons
+
+print("Each persons will pay =", output)
